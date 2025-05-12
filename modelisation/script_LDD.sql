55aaa32f436fa718afefcf05c97786b4b125bbe9
JonasBrr <jonaas.bourreau+github@gmail.com>
2025-05-12 13:59:49 +0000

    edit LDD file

--- modelisation/script_LDD.sql
+++ modelisation/script_LDD.sql
@@ -1,26 +1,23 @@
-CREATE TABLE user_(
-   id_user COUNTER,
+CREATE TABLE utilisateur (
+   id_user SERIAL PRIMARY KEY,
    email VARCHAR(50),
    nom_user VARCHAR(50),
-   prenom VARCHAR(50),
-   PRIMARY KEY(id_user)
+   prenom VARCHAR(50)
 );
 
-CREATE TABLE materiel(
-   id_materiel COUNTER,
+CREATE TABLE materiel (
+   id_materiel SERIAL PRIMARY KEY,
    nom_materiel VARCHAR(50),
-   quantite_materiel INT,
-   PRIMARY KEY(id_materiel)
+   quantite_materiel INT
 );
 
-CREATE TABLE reservation(
-   id_res COUNTER,
+CREATE TABLE reservation (
+   id_res SERIAL PRIMARY KEY,
    debut_res DATE,
    fin_res DATE,
    quantite_res INT,
    id_materiel INT,
    id_user INT,
-   PRIMARY KEY(id_res),
-   FOREIGN KEY(id_materiel) REFERENCES materiel(id_materiel),
-   FOREIGN KEY(id_user) REFERENCES user_(id_user)
+   FOREIGN KEY (id_materiel) REFERENCES materiel(id_materiel),
+   FOREIGN KEY (id_user) REFERENCES utilisateur(id_user)
 );
