03e28dd2606d4aaff88d12121f091da4b8befbad
JonasBrr <jonaas.bourreau+github@gmail.com>
2025-06-02 16:15:08 +0000

    add some sql files

--- modelisation/ex8.sql
+++ modelisation/ex8.sql
@@ -11,3 +11,32 @@ CREATE TABLE disponibilite (
     date_fin DATE NOT NULL,
     FOREIGN KEY (id_materiel) REFERENCES materiel(id_materiel)
 );
+
+-- 2. Modifiez la table "reservation" en ajoutant une nouvelle colonne "id_disponibilite" (clé étrangère référençant la table "disponibilite").
+ALTER TABLE reservation
+ADD COLUMN id_disponibilite INT,
+ADD CONSTRAINT fk_id_disponibilite
+    FOREIGN KEY (id_disponibilite)
+    REFERENCES disponibilite(id_disponibilite);
+
+-- 3. Modifiez les contraintes SQL existantes pour prendre en compte les contraintes de disponibilité lors de la création et de la mise à jour des réservations.
+ALTER TABLE reservation
+ADD CONSTRAINT chk_dates_reservation
+CHECK (fin_res > debut_res);
+
+-- 4. Implémentez une fonctionnalité permettant de vérifier la disponibilité d'un matériel pour une période donnée avant de permettre la réservation. Si le matériel n'est pas disponible, affichez un message d'erreur approprié.
+-- il faut commencer par insérer des données dans la table
+INSERT INTO disponibilite (id_materiel, date_debut, date_fin) VALUES
+  (1, CURRENT_DATE - INTERVAL '5 days',  CURRENT_DATE + INTERVAL '2 days'),
+  (2, CURRENT_DATE - INTERVAL '1 day',   CURRENT_DATE + INTERVAL '5 days'),
+  (3, CURRENT_DATE,                      CURRENT_DATE + INTERVAL '3 days'),
+  (4, CURRENT_DATE + INTERVAL '1 day',  CURRENT_DATE + INTERVAL '7 days'),
+  (5, CURRENT_DATE + INTERVAL '3 days', CURRENT_DATE + INTERVAL '10 days'),
+
+  (6, CURRENT_DATE + INTERVAL '30 days',  CURRENT_DATE + INTERVAL '45 days'),
+  (7, CURRENT_DATE + INTERVAL '40 days',  CURRENT_DATE + INTERVAL '75 days'),
+  (8, CURRENT_DATE + INTERVAL '60 days',  CURRENT_DATE + INTERVAL '85 days'),
+
+  (9,  CURRENT_DATE + INTERVAL '180 days', CURRENT_DATE + INTERVAL '210 days'),
+  (10, CURRENT_DATE + INTERVAL '200 days', CURRENT_DATE + INTERVAL '230 days'),
+  (8, CURRENT_DATE + INTERVAL '365 days', CURRENT_DATE + INTERVAL '395 days');
